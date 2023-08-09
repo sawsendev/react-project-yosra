8d4f46c27b3e04700ead525a9e5cd9253aa14f00
@taheny <t.belguith@oxton-digital.com>
2023-08-09 10:54:09 +0100

    NR-84

--- src/components/CMS/Cms.js
+++ src/components/CMS/Cms.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import "./Cms.css"
 
 const Cms = () => {
   return (
-    <div>
+    <div className='Cms-container'>
       <div className='container-fluid'>
-       <h1>Content Management System</h1>
+       <h1 className='Cms-header'>Content Management System</h1>
        <div className='Cms-content-first'>
         <h3>1. Introduction</h3>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the1500s, specimen book. It has 
@@ -22,7 +23,20 @@ survived not only five centuries  but also the leap into electronic typesetting,
         <h3>3. Lorem Ipsum</h3>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the1500s, specimen book. It has 
 survived not only five centuries  but also the leap into electronic typesetting, remaining .</p>
+        <ol type="a">
+          <li> Nom de l'article</li>
+          <li> Photos(s) de l'article</li>
+          <li> Description de l'article</li>
+          <li> Tailles et couleurs disponibles</li>
+        </ol>
        </div>
+
+       <div className='Cms-content-first'>
+        <h3>3. Lorem Ipsum</h3>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the1500s, specimen book. It has 
+survived not only five centuries  but also the leap into electronic typesetting, remaining .</p>
+       </div>
+
       </div>
     </div>
   )
