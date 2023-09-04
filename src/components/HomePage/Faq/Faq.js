0063e7c4b741b8a748a05d58c863eee0c3183931
yosr <laajimiyosr2@gmail.com>
2023-09-04 13:22:57 +0100

    faq

--- src/components/HomePage/Faq/Faq.js
+++ src/components/HomePage/Faq/Faq.js
@@ -9,16 +9,15 @@ const Faq = () => {
     window.location.href='/faq'
   };
   return (
-    <> <div className='Faq-container '> 
-    
+    <> <div className='Faq-container'> 
+       <div>
       <h3>Frequently asked questions</h3>
       <p>The most common questions that you might have before booking a room with us.</p>
     
     
-    <button className='Faq-btn' onClick={handleFaqButtonClick}>Go to our FAQ page 
-    <img  className='ms-2' src={arrow} alt='icon'/>
-    </button></div></>
+     <button className='Faq-btn' onClick={handleFaqButtonClick}>Go to our FAQ page 
+     <img  className='ms-2' src={arrow} alt='icon'/>
+     </button></div></div></>
   )
 }
-
 export default Faq
