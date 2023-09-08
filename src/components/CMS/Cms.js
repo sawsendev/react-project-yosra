d174886f3ad9b5e690cbac659dcdf882435ce8d5
yosr <laajimiyosr2@gmail.com>
2023-09-08 10:49:29 +0100

    cms

--- src/components/CMS/Cms.js
+++ src/components/CMS/Cms.js
@@ -1,14 +1,39 @@
-import React from 'react'
-import "./Cms.css"
-import Breadcrumbs from "../Breadcrumbs/Breadcrumbs"
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom';
+import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
+
 const Cms = () => {
+  const { id } = useParams(); // Obtenez l'ID à partir de l'URL
+  const [pageContent, setPageContent] = useState(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Utilize the ID to obtain the slug from the API
+    fetch(`http://dev.niceroom.sofis-info.com/api/cms/page/${id}`, { mode: 'no-cors' })
+      .then(response => response.json())
+      .then(data => {
+        setPageContent(data); 
+        if (data.slug) {
+          navigate.replace(`/cms/page/${data.slug}`);
+        }
+      })
+      .catch(error => {
+        console.error('Erreur de requête :', error);
+      
+      });
+  }, [id, navigate]);
+
+  if (!pageContent) {
+    return <p>Chargement en cours...</p>;
+  }
+
   return (
     <>
     <Breadcrumbs/>
     <div className='Cms-container'>
     
       <div className='container'>
-       <h1 className='Cms-header'>Content Management System</h1>
+       <h1 className='Cms-header'>{pageContent.title}</h1>
        <div className='Cms-content-first'>
         <h3>1. Introduction</h3>
         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the1500s, specimen book. It has 
