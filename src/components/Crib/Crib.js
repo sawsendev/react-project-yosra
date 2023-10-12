10e9a8927bdd092659cb053e6bb3f982a24459dc
Amina Ghazel <a.ghazel@oxton-digital.com>
2023-10-12 12:53:07 +0100

    #NR-144 : image carousel room

--- src/components/Crib/Crib.js
+++ src/components/Crib/Crib.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import room from '../../assets/bacdbb_723fc3ba38f34640b08464a29a8990c9_mv2-1.jpg'
+import room1 from '../../assets/img-room-1.jpg'
+import room2 from '../../assets/img-room-2.jpg'
 import { Badge } from 'react-bootstrap';
 import  locationIcon  from '../../assets/pin 2.svg';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
@@ -28,12 +29,12 @@ const Crib = ({ cribs }) => {
                   <Carousel showStatus={false} showArrows={false} showThumbs={true} dynamicHeight={false} useKeyboardArrows={false}>
                     <Link to={`/room/${crib.id}`}>
                       <div>
-                        <img className="img-fluid" src={room} alt="photo_fine_cribs" />
+                        <img className="img-fluid" src={room1} alt="photo_fine_cribs" />
                       </div>
                     </Link>
                     <Link to={`/room/${crib.id}`}>
                       <div>
-                        <img className="img-fluid" src={room} alt="photo_fine_cribs" />
+                        <img className="img-fluid" src={room2} alt="photo_fine_cribs" />
                       </div>
                     </Link>
                     {/* Add more images here as needed */}
