4afeabad19b2e5969d4cf5c9967f243cdda04f4f
Amina Ghazel <a.ghazel@oxton-digital.com>
2023-10-16 14:03:11 +0100

    #NR-153 : content de page Faq

--- src/components/Faqspage/Faqspage.js
+++ src/components/Faqspage/Faqspage.js
@@ -1,14 +1,16 @@
 import React, {useState} from 'react'
 import "./Faqspage.css"
-import {FaqspageTableRenting} from "../../Data/Data"
+import {FaqspageTableRenting, FaqspageTablePartnering} from "../../Data/Data"
 import Breadcrumbs from '../Breadcrumbs/Breadcrumbs';
 import {IoIosArrowDown, IoIosArrowUp} from 'react-icons/io'
 import {BsArrowRightShort} from 'react-icons/bs'
+import PopupAlert from '../PopupAlert/PopupAlert'
 
 const Faqspage = () => {
      const [toggleState, setToggleState]=useState(1)
      const [selected, setSelected]=useState(null)
      const [selectedAnswer, setSelectedAnswer] =useState(null)
+     const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   
      const toggleTab=(index)=>{
@@ -29,80 +31,100 @@ const Faqspage = () => {
            return setSelectedAnswer(index)
      }
 
+     const openPopup = () => {
+          setIsPopupOpen(true);
+        };
+      
+        const closePopup = () => {
+          setIsPopupOpen(false);
+        };
+        
 
-     return (
+return (
     <div>
-    <Breadcrumbs/>
-        <div className='container FaqpagesContainer'>
-          <h1 className='text-center my-5'>Frequently asked questions</h1>
-          <div className='faq-tabs'>
+     <Breadcrumbs/>
+     <div className='container FaqpagesContainer'>
+     <h1 className='text-center my-5'>Frequently asked questions</h1>
+       <div className='faq-tabs'>
           {/* tabs */}
-           <div className='bloc-tabs row m-0'>
+          <div className='bloc-tabs row m-0'>
                <div className={`col-md-6 text-center px-0 pb-3 mb-3  ${toggleState === 1 ? "active-tabs" : "tabs"}`}
                onClick={()=>toggleTab(1)}>Renting with Finecribs</div>
                <div className={`col-md-6 text-center px-0 pb-3 mb-3 ${toggleState === 2 ? "active-tabs" : "tabs"}`}
                onClick={()=>toggleTab(2)}>Partnering with Finecribs</div>
-           </div>
-           <div className='tabs-content my-3'>
-            <div  className={toggleState === 1 ? " active-content" : "content"}
-               onClick={()=>toggleTab(1)}>
-                    
-               <div className='display-block '>
-               {FaqspageTableRenting.map((renting, index) => (
-               <div key={index} className='collapseRenting my-3'>
-                    <div 
-                    className='TitlesRenting px-3 py-2 align-items-center d-flex justify-content-between'
-                    onClick={() => toggleSubTabs(index)}
-                    >
-                    <h2 className='mb-0'>{renting.title}</h2>
-                    
-                    <span>
-  {selected === index ? (
-
-    <IoIosArrowUp className='arrow'/>
-  ) : (
-     <IoIosArrowDown className='arrow'/>
-  )}
-</span>
-
-
-
+          </div>
+          <div className='tabs-content my-3'>
+               <div  className={toggleState === 1 ? " active-content" : "content"} onClick={()=>toggleTab(1)}>
+                    <div className='display-block '>
+                         {FaqspageTableRenting.map((renting, index) => (
+                              <div key={index} className='collapseRenting my-3'>
+                                   <div className='TitlesRenting px-3 py-2 align-items-center d-flex justify-content-between' onClick={() => toggleSubTabs(index)}>
+                                        <h2 className='mb-0'>{renting.title}</h2>
+                                        <span>{selected === index ? ( <IoIosArrowUp className='arrow'/> ) : ( <IoIosArrowDown className='arrow'/> )}</span>
+                                   </div>
+                                   <div className={`mx-5 subAnswers ${selected === index ? "active-content" : "content"}`}>
+                                        {renting.subTitles ? (
+                                             renting.subTitles.map((subTitle, subIndex) => (
+                                        
+                                             
+                                             <div key={subIndex} className={`pr-5 py-2 row align-items-baseline justify-content-between ${selectedAnswer === subIndex ? "subTitlesRentingActive" : "subTitlesRenting"}`}>
+                                             <h4 className='col-lg-5 col-sm-12 mb-0 py-2'
+                                             onClick={()=>toggleSubTabsAnswers(subIndex)}> <BsArrowRightShort className='fleche'/> {subTitle.title}</h4>
+                                        
+                                             <p className={`col-lg-7 col-sm-12 col-content ${selectedAnswer === subIndex ? "active-content" : "content"}`}>
+                                                  {/* {subTitle.answer}  */}
+                                                  <div dangerouslySetInnerHTML={{ __html: subTitle.answer }} />
+                                             </p>
+                                             </div>
+                                             ))
+                                        ) : (
+                                             <div>No subTitles available</div>
+                                        )}
+                                   </div>
+                              </div>
+                         ))}
                     </div>
-                    <div className={`mx-5 subAnswers ${selected === index ? "active-content" : "content"}`}>
-                    {renting.subTitles ? (
-                         renting.subTitles.map((subTitle, subIndex) => (
-                     
-                         
-                         <div key={subIndex} className={`pr-5 py-2 row align-items-baseline justify-content-between ${selectedAnswer === subIndex ? "subTitlesRentingActive" : "subTitlesRenting"}`}>
-                         <h4 className='col-lg-5 col-sm-12 mb-0 py-2'
-                         onClick={()=>toggleSubTabsAnswers(subIndex)}> <BsArrowRightShort className='fleche'/> {subTitle.title}</h4>
-                    
-                         <p className={`col-lg-7 col-sm-12 col-content ${selectedAnswer === subIndex ? "active-content" : "content"}`}>{subTitle.answer}</p>
-                         </div>
-                         ))
-                    ) : (
-                         <div>No subTitles available</div>
-                    )}
-                    </div>
-               </div>
-               ))}
-               </div>
 
-            </div>
+               </div>
+          {/* end tab-content renting Tab 1 */}
+               <div  className={toggleState === 2 ? " active-content" : "content"} onClick={()=>toggleTab(2)}>
+                    <div className='display-block '>
+                         {FaqspageTablePartnering.map((renting, index) => (
+                                   <div className={`mx-5 subAnswers `}>
+                                        {renting.subTitles ? (
+                                             renting.subTitles.map((subTitle, subIndex) => (
+                                        
+                                             
+                                             <div key={subIndex} className={`pr-5 py-2 row align-items-baseline justify-content-between ${selectedAnswer === subIndex ? "subTitlesRentingActive" : "subTitlesRenting"}`}>
+                                             <h4 className='col-lg-5 col-sm-12 mb-0 py-2'
+                                             onClick={()=>toggleSubTabsAnswers(subIndex)}> <BsArrowRightShort className='fleche'/> {subTitle.title}</h4>
+                                        
+                                             <p className={`col-lg-7 col-sm-12 col-content ${selectedAnswer === subIndex ? "active-content" : "content"}`}>
+                                                  {/* {subTitle.answer}  */}
+                                                  <div dangerouslySetInnerHTML={{ __html: subTitle.answer }} />
+                                             </p>
+                                             </div>
+                                             ))
+                                        ) : (
+                                             <div>No content available</div>
+                                        )}
+                                   </div>
+                         ))}
+                    </div>
+
+               </div>
             
 
 
           
-            <div className={toggleState === 2 ? " active-content" : "content"}>
-               <h2>content2</h2>
-               <p>Lorem ipsum is placeholder text commonly used in the graphic, 
-            print, and publishing industries for previewing layouts and visual mockups</p>
-            </div>
 
            </div>
+
         </div>
     </div>
+      <PopupAlert isPopupOpen={isPopupOpen} onClose={closePopup}/>
     </div>
+    
   )
 }
 
