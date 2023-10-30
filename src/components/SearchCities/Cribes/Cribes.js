7f54717ee764db1c6e9637c78db70173bf526743
yosr <laajimiyosr2@gmail.com>
2023-10-30 12:02:42 +0100

    scroll

--- src/components/SearchCities/Cribes/Cribes.js
+++ src/components/SearchCities/Cribes/Cribes.js
@@ -1,8 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 import './Cribes.css';
 import AlertCribes from '../AlertCribes/AlertCribes';
 import Crib from '../../Crib/Crib';
-import InfiniteScroll from 'react-infinite-scroll-component';
 import { useLocation, useNavigate } from 'react-router-dom';
 import noRooms from "../../../assets/Group 24.svg";
 import CribMap from '../MapContainer/CribMap';
@@ -11,8 +10,9 @@ const Cribes = () => {
   const [cribsData, setCribsData] = useState([]);
   const [searchResult, setSearchResult] = useState([]);
   const [staticCoordinates, setStaticCoordinates] = useState([]);
-  const [itemsToDisplay, setItemsToDisplay] = useState(9);
   const [dataLoaded, setDataLoaded] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [lastPage, setLastPage] = useState(1);
 
   const API_KEY = 'a2b18f9cfb72eb93f3ce6b1c30372b59';
   const API_URL = 'http://dev.niceroom.sofis-info.com/api/lots/list';
@@ -29,27 +29,25 @@ const Cribes = () => {
   const sortByParam = searchParams.get('sortBy');
   const searchParamsExist = cityParam || dateParam || priceMinParam || priceMaxParam || sortByParam;
   const itemsPerPage = 9;
-  const [lastPage, setLastPage] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
-  const fetchDataFromAPI = async () => {
+
+  const fetchDataFromAPI = async (page) => {
     try {
       const headers = {
         'apiKey': `${API_KEY}`,
       };
-      const response = await fetch(`${API_URL}?page=${currentPage}`, {
+      const response = await fetch(`${API_URL}?page=${page}`, {
         method: 'GET',
         mode: 'cors',
         headers,
       });
 
       const data = await response.json();
-      console.log(data);
 
+      setLastPage(data.data.lots.last_page);
+      
       if (currentPage === 1) {
         // Si c'est la première page, réinitialisez les données
         setCribsData(data.data.lots.data);
-        // Mettez à jour lastPage avec la valeur de la dernière page
-        setLastPage(data.data.lots.last_page);
       } else {
         // Sinon, ajoutez les nouvelles données
         setCribsData((prevData) => [...prevData, ...data.data.lots.data]);
@@ -59,35 +57,6 @@ const Cribes = () => {
       console.error('Erreur lors de la récupération des données :', error);
     }
   };
-
-  useEffect(() => {
-    if (!searchParamsExist) {
-      // Réinitialisez la page lorsque les paramètres de recherche changent
-      setCurrentPage(1);
-      setItemsToDisplay(9); // Réinitialisez le nombre d'éléments à afficher
-      navigate('/search-cities');
-      fetchDataFromAPI();
-    }
-  }, [searchParamsExist]);
-
-  useEffect(() => {
-    // Mettez à jour la page lorsque vous atteignez la fin des données
-    if (itemsToDisplay >= (searchParamsExist ? searchResult.length : cribsData.length)) {
-      // Vérifiez si vous avez encore des pages à charger
-      if (currentPage < lastPage) {
-        // Si oui, augmentez la page actuelle et chargez les données
-        fetchDataFromAPI();
-      }
-    }
-  }, [itemsToDisplay, currentPage, lastPage]);
-
-  const fetchNextPage = async () => {
-    if (currentPage <= lastPage) {
-      setCurrentPage((prevPage) => prevPage + 1);
-     
-    }
-  };
-
   const fetchDataFromAPI2 = async (page) => {
     if (searchParamsExist) {
       const formData = {
@@ -110,32 +79,58 @@ const Cribes = () => {
 
         if (response.ok) {
           const data = await response.json();
-          console.log(data);
+          setLastPage(data.data.lots.last_page);
 
-          if (page === 1) {
+          if (currentPage === 1) {
             // Si c'est la première page, réinitialisez les données de recherche
-            setSearchResult(data.lots.data);
+            setSearchResult(data.data.lots.data);
           } else {
             // Sinon, ajoutez les nouvelles données de recherche
-            setSearchResult((prevData) => [...prevData, ...data.lots.data]);
+            setSearchResult((prevData) => [...prevData, ...data.data.lots.data]);
           }
 
-          const extractedCoordinates = data.lots.data.map(crib => {
-            const latitude = crib.apartment.building.latitude;
-            const longitude = crib.apartment.building.longitude;
-            return [latitude, longitude];
-          });
-
-          setStaticCoordinates(extractedCoordinates);
-        } else {
-          console.error('Erreur lors de la requête POST');
         }
-      } catch (error) {
+         setDataLoaded(true);
+        }catch (error) {
         console.error('Erreur lors de la récupération des données :', error);
       }
     }
   };
 
+  const handleScroll = (currentPage, lastPage, setCurrentPage) => {
+    if (
+      window.innerHeight + document.documentElement.scrollTop ===
+      document.documentElement.offsetHeight
+    ) {
+      if (currentPage < lastPage) {
+        setCurrentPage(currentPage + 1);
+      }
+    }
+  };
+  
+ console.log(currentPage)
+ console.log(lastPage)
+  useEffect(() => {
+    if (!searchParamsExist) {
+      navigate('/search-cities');
+      fetchDataFromAPI(currentPage);
+    }
+    else{
+      fetchDataFromAPI2(currentPage)
+    }
+  }, [currentPage,searchParamsExist]);
+
+  useEffect(() => {
+    const onScroll = () => {
+      handleScroll(currentPage, lastPage, setCurrentPage);
+    };
+  
+    window.addEventListener('scroll', onScroll);
+  
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, [currentPage, lastPage, setCurrentPage]);
 
   return (
     <div className='Cribes-container container-fluid'>
@@ -151,38 +146,30 @@ const Cribes = () => {
         <p>
           Loading ...
         </p>
-      )}
+     ) }
 
       <div className='content-page'>
         <div className='row row-cribes'>
           <div className='col-lg-7'>
-          <InfiniteScroll
-              dataLength={itemsToDisplay}
-              next={fetchNextPage} // Chargez davantage d'éléments
-              hasMore={currentPage <= lastPage}
-              loader={<p>Loading ...</p>} // Affichez un indicateur de chargement
-              style={{ overflowX: 'hidden' }}
-            >
-              {searchParamsExist && dataLoaded ? (
-                searchResult.length > 0 ? (
-                  <Crib cribs={searchResult} />
-                ) : (
-                  <div className='container'>
-                    <div className='No-rooms-content'>
-                      <div className='left d-flex '>
-                        <img className='ImageNoRooms' src={noRooms} alt='no rooms icon' />
-                        <span>No rooms available</span>
-                        <button className='button'>Show first availabilities</button>
-                      </div>
-                    </div>
-                  </div>
-                )
+            {dataLoaded ? (
+              searchParamsExist && searchResult.length > 0 ? (
+                <Crib cribs={searchResult} />
               ) : (
-                dataLoaded && (
-                  <Crib cribs={cribsData} />
-                )
-              )}
-            </InfiniteScroll>
+                <Crib cribs={cribsData} />
+              )
+            ) : null}
+
+            {dataLoaded && cribsData.length === 0 && searchResult.length === 0 && (
+              <div className='container'>
+                <div className='No-rooms-content'>
+                  <div className='left d-flex '>
+                    <img className='ImageNoRooms' src={noRooms} alt='no rooms icon' />
+                    <span>No rooms available</span>
+                    <button className='button'>Show first availabilities</button>
+                  </div>
+                </div>
+              </div>
+            )}
           </div>
 
           <div className='Maps col-lg-5'>
