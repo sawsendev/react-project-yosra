6456088a35f8fb32afe0de6266cca09d7119c1bc
yosr12 <y.laajimi@oxton-digital.com>
2023-11-28 11:14:49 +0100

    NR-210/NR-225

--- src/components/SearchCities/CityResult/CityResult.js
+++ src/components/SearchCities/CityResult/CityResult.js
@@ -4,13 +4,20 @@ const CityResult = ({city}) => {
 const capitalizedCity = city.charAt(0).toUpperCase() + city.slice(1);
 const backgroundImage = require(`../../../assets/${city}.png`);
 
+const cityTexts = {
+  nice: "Sunny boulevards, international allure, and Riviera charm",
+  bologna: "A town of hidden gems",
+  florence: "Lose yourself as you wander through the city's narrow lanes",
+};
+const cityDescription = cityTexts[city.toLowerCase()] || "";
+
   return (
     <div className='page-container-city'  style={{ backgroundImage: `url(${backgroundImage})` }} >
 
     <div className='container'>
       <div className='content-container-city'>
         <h1>{capitalizedCity}</h1>
-        <p>A place where you learn to become a local. A town of hidden gems</p>
+        <p>{cityDescription}</p>
       </div>
     </div>
   </div>
